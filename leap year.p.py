31c8dc0eca5c492ded342f444d96d4311bcf4355
siddhi2832007 <siddhikadam09141@gmail.com>
2026-02-12 11:28:44 +0530

    Add files via upload

---
+++ leap year.p.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 10:00:36 2026
+
+@author: User
+"""
+
+year = int(input("Enter year:"))
+if (year % 400==0) or (year% 4==0 and year % 100!=0):
+        print("Leap year")
+        
+else:
+     print("Not a Leap Year")

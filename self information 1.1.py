9fd277356d7ed3428b9e5349fc01a64a7551fd64
siddhi2832007 <siddhikadam09141@gmail.com>
2026-02-12 11:29:00 +0530

    Add files via upload

---
+++ self information 1.1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 11:04:44 2026
+
+@author: User
+"""
+
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+student_class=input("Enter your class: ")
+division=input("Enter your division: ")
+
+print("\n-----student information------")
+
+
+print("Name:",name)
+print("age:",age)
+print("class:",student_class)
+print("division:",division)
+

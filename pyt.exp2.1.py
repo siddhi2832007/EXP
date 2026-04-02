2d5757bfa07c04e5146546e99208e8a4c5b68045
siddhi2832007 <siddhikadam09141@gmail.com>
2026-04-02 18:07:03 +0530

    Update pyt.exp2.1.py

--- pyt.exp2.1.py
+++ pyt.exp2.1.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Apr  2 10:33:10 2026
 
-@author: User
+@author: siddhi kadam
 """
 year = int(input("Enter year: "))
 if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):

fc536e7fdb72d099cb638fe7db5e82eb11e2bf8a
siddhi2832007 <siddhikadam09141@gmail.com>
2026-04-02 11:56:20 +0530

    Add files via upload

---
+++ pyt.exp2.3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 11:52:21 2026
+
+@author: User
+siddhi kadam
+"""
+
+speed =  int(input("Enter speed km/h:"))
+if speed>60:
+    print("Overspeeding!Fine Applied.")
+else:
+    print("Within Limit.")

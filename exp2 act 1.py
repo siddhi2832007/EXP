0c4295c630fe4da0d45348f87c4c4bc7be11dd43
siddhi2832007 <siddhikadam09141@gmail.com>
2026-05-07 10:57:40 +0530

    Add files via upload

---
+++ exp2 act 1.py
@@ -0,0 +1,15 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 13:47:47 2026
+
+@author: siddhi kadam
+"""
+
+salary = int(input("Enter your salary: "))
+
+if salary >= 25000:
+    print("Loan Approved")
+else:
+    print("Loan Rejected")

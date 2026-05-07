35e4515eb561e2e58ae3a8f4aef9433d5b2fd6b6
siddhi2832007 <siddhikadam09141@gmail.com>
2026-05-07 11:00:45 +0530

    Add files via upload

---
+++ exp2 act 2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 13:55:03 2026
+
+@author: siddhi kadam
+"""
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Entry Allowed")
+else:
+    print("Entry Denied")
